40cf70c6f8561f8e1f0523585bae43a8c6d091aa
Anupam <anupamkmr5200@gmail.com>
2024-07-05 00:13:43 +0530

    [Anupam] completed: CRUD operations using mongodb and ejs

--- app.js
+++ app.js
@@ -9,29 +9,79 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,"public")));
 // static files are in public
 
-app.get('/',(req,res)=>
+app.get('/create',(req,res)=>
 {
-    res.render("index"); 
+    res.render("create"); 
 })
 
-app.get('/create', async (req,res)=>
+
+// CREATE 
+
+app.post('/create', async (req,res)=>
 {
     let {name, email, image} = req.body;   // same name as in create.ejs
     
-    let createdUser = await userModel.create({
-        name,   // same as name: name
-        email,
-        image
+     await userModel.create({
+        name:name,   // same as name: name // name (schema) : name (req.body.name)
+        email:email,
+        image:image
     });
 
-    res.send(createdUser);
+
+    // res.send(createdUser);
+    res.redirect("/user");
 })
 
 app.get('/user', async (req,res)=>
 {
-    let allUsers = await userModel.find()
-    res.render("users",{users:allUsers})
+    let users = await userModel.find()
+    res.render("users",{users});
 })
+
+
+
+// UPDATE
+app.get('/edit/:userid', async (req,res)=>
+{        
+    let user = await userModel.findOne({_id:req.params.userid});
+    res.render("update", {user});
+})
+
+
+app.post('/update/:userid', async(req,res)=>{
+ 
+    let {name, email, image} = req.body;
+    let user = await userModel.findOneAndUpdate({_id:req.params.userid}, {name, email, image}, {new:true});
+    res.redirect("/user");
+})
+
+
+
+
+// READ
+
+app.get('/read', async (req,res)=>
+{
+    const user = await userModel.find({email: "anupam@gmail.com"});
+     res.send(user);
+})
+
+app.get('/readAll', async (req,res)=>
+{
+    const allusers = await userModel.find();
+    res.send(allusers);
+
+})
+
+
+// DELETE
+app.get('/delete/:userid', async(req,res)=>
+{
+    await  userModel.findOneAndDelete({_id:req.params.userid});
+    res.redirect("/user");
+})
+
+
 
 app.listen(3000, (err)=>
 {
